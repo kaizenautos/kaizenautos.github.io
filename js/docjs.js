edb88171e53a569fdab9e9c4e7e8b0d1258f8a81
Kaizenauto <asutherland@webdeaslja.com>
2019-11-19 13:46:07 -0500

    Moved document srcipt to it's own file

---
+++ js/docjs.js
@@ -0,0 +1,13 @@
+	$('#myModal').show();
+	//Opens a new tab with the value from the div id rental
+	var openDoc = function(){
+		var divText = document.getElementById("rental").outerHTML;
+		//var btn = document.getElementById("sBtn").outerHTML;
+		var myWindow = window.open();
+		var doc = myWindow.document;
+		doc.open();
+		doc.write("<img src='media/kaizen.png' style='width: 19%;height:21%;'><span style='float:right; color:silver;font-size: 25px;font-weight: bold;font-family:courier,arial,helvetica;' id='title'>Kaizen Auto Brokers LTD<br/>Rental Agreement</span><hr>")
+		doc.write(divText);
+		doc.write("<button style='width:100px' onclick='window.print()'>Print</button>");
+		doc.close();
+	}
